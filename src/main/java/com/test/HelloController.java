60c69cfc78bf5b1149a602b2ead99396527e1748
Roel Cousijnse <cousijnse@holmes.nl>
2020-06-24 11:32:26 +0200

    first setup test program

---
+++ src/main/java/com/test/HelloController.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+}
